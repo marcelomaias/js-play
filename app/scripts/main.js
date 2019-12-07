faf78c4b6972f8e0d893ce4e3866a9b34a38f89b
Maia <maia@Citadel.local>
2019-12-07 19:11:33 -0300

    Initial Commit

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,5 +1,24 @@
-import { banana } from './components/component';
+import Bouncer from "formbouncerjs";
+import messages from "./components/formMessages";
 
-console.log('\'Allo \'Allo!');
+var validate = new Bouncer("form", {
+  messages: messages,
+  // Form Submission
+  disableSubmit: true, // If true, native form submission is suppressed even when form validates
 
-banana();
+  // Custom Events
+  emitEvents: true // If true, emits custom events
+});
+
+// Detect a successful form validation
+document.addEventListener(
+  "bouncerFormValid",
+  function(event) {
+    // The successfully validated form
+    var form = event.target;
+
+    console.log(form);
+    // If `disableSubmit` is true, you might use this to submit the form with Ajax
+  },
+  false
+);
