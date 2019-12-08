c3bd11776b3980102a7a2b2c9eb238a60b08a514
Maia <maia@Citadel.local>
2019-12-08 01:58:06 -0300

    Initial Commit

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,24 +1,50 @@
 import Bouncer from "formbouncerjs";
 import messages from "./components/formMessages";
 
-var validate = new Bouncer("form", {
+let items = [];
+
+const validate = new Bouncer("form", {
   messages: messages,
   // Form Submission
   disableSubmit: true, // If true, native form submission is suppressed even when form validates
-
   // Custom Events
   emitEvents: true // If true, emits custom events
 });
-
 // Detect a successful form validation
 document.addEventListener(
   "bouncerFormValid",
-  function(event) {
+  function(e) {
     // The successfully validated form
-    var form = event.target;
-
-    console.log(form);
+    const form = e.target;
     // If `disableSubmit` is true, you might use this to submit the form with Ajax
+    addItem(form);
   },
   false
 );
+
+function addItem(form) {
+  let item = {
+    id: Date.now(),
+    nome: form.elements.name.value,
+    email: form.elements.email.value
+  };
+  items.push(item);
+  form.reset();
+  localStorage.setItem("Items", JSON.stringify(items));
+  printItem(items);
+}
+
+const printItem = els => {
+  const theItems = els
+    .map(item => {
+      return `
+    <h2>${item.nome}</h2>
+    <p>${item.email}</p>
+  `;
+    })
+    .join(" ");
+
+  const container = document.getElementById("container");
+  container.innerHTML = theItems;
+  document.body.append(container);
+};
